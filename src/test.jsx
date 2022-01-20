24837126250e094d50955673b8ecb23f6a8d9e46
ingmin <ymkwon3@gmail.com>
2022-01-21 00:43:47 +0900

    chapter2

--- src/test.jsx
+++ src/test.jsx
@@ -1,5 +1,20 @@
+
+function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+    firstName: 'Harper',
+    lastName: 'Perez'
+};
+
+const element = (
+    <h1>
+        Hello, {formatName(user)}!
+    </h1>
+);
+
 ReactDOM.render(
-    <h1>Hello, world!</h1>,
+    element,
     document.getElementById('hello_container')
 );
-// const domContainer = document.querySelector('#hello_container');
