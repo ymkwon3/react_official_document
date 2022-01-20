ceac1cc57709cd1119a03a493de32e157e2c78c5
ingmin <ymkwon3@gmail.com>
2022-01-20 23:48:45 +0900

    init

---
+++ src/test.jsx
@@ -0,0 +1,5 @@
+ReactDOM.render(
+    <h1>Hello, world!</h1>,
+    document.getElementById('hello_container')
+);
+// const domContainer = document.querySelector('#hello_container');
---
+++ test.js
@@ -0,0 +1,6 @@
+ReactDOM.render(React.createElement(
+    'h1',
+    null,
+    'Hello, world!'
+), document.getElementById('hello_container'));
+// const domContainer = document.querySelector('#hello_container');
