7806b7af5604fc0fd8e049ed11761fa12a98dc51
ingmin <ymkwon3@gmail.com>
2022-01-23 23:59:23 +0900

    chapter4

--- test.js
+++
@@ -1,6 +0,0 @@
-ReactDOM.render(React.createElement(
-    'h1',
-    null,
-    'Hello, world!'
-), document.getElementById('hello_container'));
-// const domContainer = document.querySelector('#hello_container');
