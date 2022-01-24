2c7520b9fb92393b2fbdcd984e0868add89c5fef
ingmin <ymkwon3@gmail.com>
2022-01-24 17:55:42 +0900

    chapter5~6

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import Toggle from "./study/Toggle"
+
 function Welcome(props) {
   return <h1>Hello, {props.name}</h1>;
 }
@@ -5,9 +7,7 @@ function Welcome(props) {
 function App() {
   return (
     <div>
-      <Welcome name="Sara" />
-      <Welcome name="Cahal" />
-      <Welcome name="Edite" />
+      <Toggle></Toggle>
     </div>
   );
 }
